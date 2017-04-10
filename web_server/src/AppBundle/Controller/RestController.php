fb7c48a98833f79c62eceb3e48a462afe53c7953
Adrian Ispas <adi.ispas95@gmail.com>
2017-04-10 13:29:05 +0300

    Update.

---
+++ web_server/src/AppBundle/Controller/RestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+abstract class RestController extends FOSRestController
+{
+    /**
+     * @param $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     */
+    protected function getOr404($id)
+    {
+//        $handler = $this->getHandler();
+//        $data = $handler->get($id);
+
+        $data = null;
+        if (null === $data) {
+            throw new NotFoundHttpException();
+        }
+        return $data;
+    }
+}
